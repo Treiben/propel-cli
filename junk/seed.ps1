ba58f982138eba35b507e25de5ec1c9621412ca3
Tatyana Asriyan <tasriyan@hotmail.com>
2025-10-07 22:51:12 -0500

    deleted junk folder

--- junk/seed.ps1
+++
@@ -1,4 +0,0 @@
-& ".\release\propel-cli.exe" seed `
-    --provider postgresql `
-    --connection-string "Host=localhost;Port=5432;Database=propel_feature_flags;Search Path=dashboard;Username=propel_user;Password=propel_password;Include Error Detail=true" `
-    --seeds-path ".\scripts\postgresql\data"
